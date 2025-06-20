37576d7a55e1cecc6e2b84cfa058376a1d3871af
Mutter45 <2996806099@qq.com>
2025-06-20 10:09:26 +0800

    feat: 更新配置

--- apps/live-app/src/App.vue
+++ apps/live-app/src/App.vue
@@ -2,13 +2,15 @@
 import { say } from '@qg/utils'
 
 const message: string = 'Hello, Live App!'
+
 say(message)
 localStorage.setItem('liveAppMessage', message)
 </script>
 
 <template>
   <div class="app">
-    <h1>🎥 Live App test1111 2222</h1>
+    <h1>🎥 Live App test1111 33334444</h1>
+    <h2>{{ message }}</h2>
   </div>
 </template>
 
---
+++ commitlint.config.mjs
@@ -0,0 +1,53 @@
+export default {
+  ignores: [commit => commit.includes('init'), commit => commit === ''],
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [1, 'always'],
+    'header-max-length': [2, 'always', 108],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+    'subject-case': [0],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'perf',
+        'style',
+        'docs',
+        'test',
+        'refactor',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+        'wip',
+        'workflow',
+        'types',
+        'release',
+      ],
+    ],
+  },
+  /*
+   * Whether commitlint uses the default ignore rules, see the description above.
+   */
+  defaultIgnores: true,
+  /*
+    * Custom URL to show upon failure
+    */
+  helpUrl:
+     'https://github.com/conventional-changelog/commitlint/#what-is-commitlint',
+  /*
+   * Custom prompt configs
+   */
+  prompt: {
+    messages: {},
+    questions: {
+      type: {
+        description: 'please input type:',
+      },
+    },
+  },
+}
